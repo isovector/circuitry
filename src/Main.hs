0ef0075366634c0427af20be5635adce1af6d2d3
Sandy Maguire <sandy@sandymaguire.me>
2016-10-23 02:14:27 -0700

    label components

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@
 module Main where
 
 import Control.Lens hiding ((#), at)
+import Data.List (zipWith)
 import Diagrams.TwoD.Arrow
 import Diagrams.TwoD.Arrowheads
 import Diagrams.Prelude
@@ -13,7 +14,7 @@ import Diagrams.TwoD.Shapes
 import Diagrams.Backend.Canvas.CmdLine
 
 dualInput :: IsName a => a -> Diagram B
-dualInput name = input 1 "in1" <> input (-1) "in2"
+dualInput name = input 1 "in0" <> input (-1) "in1"
   where
     input d n = (mkCon (name, n) ||| inputLine) # translate (r2 (0, spacing * d))
     spacing = 0.25
@@ -26,31 +27,43 @@ andGate name = dualInput name
            ||| roundedRect' 1.2 1 (with & radiusBR .~ 0.5
                                         & radiusTR .~ 0.5
                                         )
-           ||| mkCon (name, "out")
+           ||| mkCon (name, "out0")
 
 spacer :: Diagram B
 spacer = rect 0.5 0.5 # lc white
 
+sspacer :: Diagram B
+sspacer = rect 0.25 0.25 # lc white
+
+labelSize = 0.3
+textSize = 0.2
+
 machine :: IsName a => a -> [String] -> [String] -> String -> Diagram B
-machine name ins outs labelText = inputStack ||| (rect width height <> inLabels <> outLabels <> label) ||| outputStack
+machine name ins outs labelText = inputNumStack ||| inputStack ||| (rect width height <> inLabels <> outLabels <> label) ||| outputStack ||| outputNumStack
   where
-    vspacing = 1.9
+    vspacing = 2.5
     hspacing = width / 2 - textSize
-    labelSize = 0.3
-    textSize = 0.2
     width = (fromIntegral ( maximum (fmap length ins)
                           + maximum (fmap length outs)) * textSize)
           + (fromIntegral (length labelText) * labelSize) + 0.5
     label = text labelText # scale labelSize
-    height = max (heightOf outs) (heightOf ins) + 0.3
+    height = min (heightOf outs) (heightOf ins)
     -- TODO(sandy): this is negative. wtf?
     heightOf ls = -vspacing * fromIntegral (length ls - 1) / 2
     stack as = foldl (\b a -> b # translate (r2 (0, vspacing)) <> a) nothing as
-    inputStack = stack (fmap (\a -> mkCon (name, a) ||| inputLine) ins) # translate (r2 (0, heightOf ins)) # scaleY textSize
-    outputStack = stack (fmap (\a -> mkCon (name, a)) outs) # translate (r2 (0, heightOf outs)) # scaleY textSize
-    textStack ls = stack (fmap text ls) # translate (r2 (0, heightOf ls)) # scale textSize
+
+    objStack as f = stack (fmap f as) # translate (r2 (0, heightOf as)) # scaleY textSize
+
+    inputNumStack  = objStack (renumber ins) $ \a -> mkCon (name, "in" ++ a)
+    inputStack     = objStack ins $ \a -> mkCon (name, a) ||| inputLine
+    outputStack    = objStack outs $ \a -> mkCon (name, a)
+    outputNumStack = objStack (renumber outs) $ \a -> mkCon (name, "out" ++ a)
+    textStack ls   = stack (fmap text ls) # translate (r2 (0, heightOf ls)) # scale textSize
+
     inLabels  = textStack ins # translate (r2 (-hspacing, 0))
     outLabels = textStack outs # translate (r2 (hspacing, 0))
+
+    renumber = zipWith ((show .) . const) [0..]
 
 polyIn :: Diagram B
 polyIn = pline 3 <> pline 2 <> pline 1
@@ -69,16 +82,29 @@ nothing = pointDiagram $ mkP2 0 0
 mkCon :: IsName a => a -> Diagram B
 mkCon name = nothing # named name
 
+con :: IsName a => a -> Diagram B
+con name = circle 0.05 # fc black # named name
+
 putAt :: IsName a => Diagram B -> a -> Diagram B -> Diagram B
 putAt what name = withName name $ \b d -> moveTo (location b) what <> d
 
-test = (machine "sr" ["S", "R", "W"] ["Q", "Q'", "b", "a"] "SR"
-     ||| polyIn
+test = (labeled "m" (machine "sr" ["S", "R"] ["Q", "Q'"] "SR"
      ||| spacer
      ||| andGate "a"
+     )
+     ||| spacer
      ||| notGate "not"
-     ||| andGate "b") # connect' headless ("sr", "Q") ("a", "in1")
-                      # putAt polyIn ("sr", "S")
+     ) # connect' headless ("sr", "out1") ("a", "in1")
+       # putAt polyIn ("sr", "S")
+       # putAt (con "split") ("a", "in1")
+       # connect' headless "split" ("a", "in0")
+       # connect' headless ("a", "out0") ("not", "in0")
+
+labeled :: String -> Diagram B -> Diagram B
+labeled label d = ( d # center # translate (r2 (-0.25, 0.125))
+                 <> rect (width d) (height d + 0.25) # lw veryThick
+                  ) === sspacer === text label # scale labelSize
+
 
 headless = with & arrowHead .~ noHead
 
