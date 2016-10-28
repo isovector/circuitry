b36f31f34cc62bd75bedfb97ae0382db3c4abfcc
Sandy Maguire <sandy@sandymaguire.me>
2016-10-27 20:18:32 -0700

    data url exporting

--- src/Circuitry/Backend.hs
+++ src/Circuitry/Backend.hs
@@ -1,3 +1,18 @@
-module Circuitry.Backend (B, mainWith) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Diagrams.Backend.Canvas.CmdLine
+module Circuitry.Backend (B, toDataURL) where
+
+import           Data.ByteString as BS
+import qualified Data.ByteString.Base64.URL as URL
+import           Data.String.Conv
+import           Diagrams.Backend.SVG
+import           Diagrams.Prelude
+import           Graphics.Svg.Core
+
+toDataURL :: Diagram B -> BS.ByteString
+toDataURL = (BS.append "data:image/svg+xml;base64,")
+          . URL.encode
+          . toS
+          . renderBS
+          . renderDia SVG (SVGOptions (mkWidth 250) Nothing "" [] True)
+
