c5fa01ca31f8c0eb828366dd4216982fe89b2632
Sandy Maguire <sandy@sandymaguire.me>
2021-05-17 21:14:07 -0700

    addressable  and addressable1 agree with lifting

--- src/Circuitry/Catalyst.hs
+++ src/Circuitry/Catalyst.hs
@@ -1,36 +1,35 @@
-{-# LANGUAGE AllowAmbiguousTypes        #-}
-{-# LANGUAGE CPP                        #-}
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE DerivingStrategies         #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase                 #-}
-{-# LANGUAGE OverloadedLists            #-}
-{-# LANGUAGE RecursiveDo                #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TupleSections              #-}
-{-# LANGUAGE TypeApplications           #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE TypeOperators              #-}
-{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE AllowAmbiguousTypes                    #-}
+{-# LANGUAGE CPP                                    #-}
+{-# LANGUAGE DataKinds                              #-}
+{-# LANGUAGE DefaultSignatures                      #-}
+{-# LANGUAGE DeriveFunctor                          #-}
+{-# LANGUAGE DeriveGeneric                          #-}
+{-# LANGUAGE DerivingStrategies                     #-}
+{-# LANGUAGE FlexibleContexts                       #-}
+{-# LANGUAGE FlexibleInstances                      #-}
+{-# LANGUAGE GADTs                                  #-}
+{-# LANGUAGE GADTs                                  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving             #-}
+{-# LANGUAGE InstanceSigs                           #-}
+{-# LANGUAGE LambdaCase                             #-}
+{-# LANGUAGE MultiParamTypeClasses                  #-}
+{-# LANGUAGE NoStarIsType                           #-}
+{-# LANGUAGE OverloadedLists                        #-}
+{-# LANGUAGE PolyKinds                              #-}
+{-# LANGUAGE RecursiveDo                            #-}
+{-# LANGUAGE ScopedTypeVariables                    #-}
+{-# LANGUAGE StandaloneDeriving                     #-}
+{-# LANGUAGE TupleSections                          #-}
+{-# LANGUAGE TypeApplications                       #-}
+{-# LANGUAGE TypeFamilies                           #-}
+{-# LANGUAGE TypeOperators                          #-}
+{-# LANGUAGE UndecidableInstances                   #-}
+{-# LANGUAGE UndecidableSuperClasses                #-}
 
-{-# OPTIONS_GHC -Wall                   #-}
-{-# OPTIONS_GHC -Wno-orphans            #-}
-{-# OPTIONS_GHC -Wno-unused-do-bind     #-}
-{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wall                               #-}
 {-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE UndecidableSuperClasses #-}
-{-# OPTIONS_GHC -Wno-missing-methods #-}
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE DefaultSignatures #-}
+{-# OPTIONS_GHC -Wno-unused-do-bind                 #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Circuitry.Catalyst
   ( module Circuitry.Category
@@ -43,17 +42,85 @@ import           Circuitry.Category
 import           Circuitry.GraphViz
 import           Clash.Sized.Vector (Vec(..))
 import qualified Clash.Sized.Vector as V
+import           Control.Arrow (first)
 import           Control.Monad.Writer (tell)
 import           Data.Bool (bool)
-import           Data.Function (fix)
+import           Data.Function (fix, on)
 import           Data.Kind (Type, Constraint)
 import           Data.Profunctor.Choice (unleft)
 import           Data.Profunctor.Types
 import           Data.Typeable (Typeable)
+import           Data.Word (Word8)
 import           GHC.Generics hiding (S)
 import           GHC.TypeLits
 import           Numeric.Natural (Natural)
 import           Prelude hiding (id, (.), sum, zip)
+import           Test.QuickCheck (CoArbitrary, Testable (property), Arbitrary (arbitrary), counterexample, applyFun, Function (function), functionMap, forAllShrink, shrink, (===), quickCheck)
+import           Test.QuickCheck.Arbitrary (CoArbitrary(coarbitrary))
+import           Test.QuickCheck.Checkers
+import qualified Data.Bits as B
+import Test.QuickCheck.Property (Property)
+import Debug.Trace (trace)
+import Data.Type.Equality
+import Unsafe.Coerce (unsafeCoerce)
+
+
+
+instance (Show a, EqProp b, Arbitrary a, Show b) => EqProp (Circuit a b) where
+  (=-=) = (=-=) `on` lowerCircuit
+
+instance Arbitrary Natural where
+  arbitrary = fmap (fromIntegral . abs @Integer) arbitrary
+
+instance CoArbitrary Natural where
+  coarbitrary = coarbitrary @Integer . fromIntegral
+
+instance Function Natural where
+  function = functionMap fromIntegral (fromIntegral @Integer . abs)
+
+instance (Show a, EqProp b, CoArbitrary r, Arbitrary a, Arbitrary r, Show r, Show b, Function r) => EqProp (Roar r a b) where
+  Roar r1 =-= Roar r2 = property $ do
+    t <- arbitrary
+    pure $ forAllShrink arbitrary shrink $ \f' -> do
+    let f = applyFun f'
+        v1 = r1 f t
+        v2 = r2 f t
+    counterexample (show t) $ counterexample (show v1) $ counterexample (show v2) $ v1 =-= v2
+
+
+embedTest :: forall a. (Eq a, Show a, Arbitrary a, Embeddable a) => Property
+embedTest = property $ do
+  a <- arbitrary @a
+  pure $ counterexample (show a) $ a === reify (embed a)
+
+instance EqProp Word8 where
+  (=-=) = (===)
+
+
+addressable1Test :: Property
+addressable1Test = property $ do
+  f <- arbitrary @(Time -> (Bool, (Bool, Word8)))
+  t <- arbitrary
+  pure $
+    runCircuit (addressable1 snapN) ((t, ) . f)
+      =-=
+        runCircuit snapN f
+
+addressableTest :: Property
+addressableTest = property $ do
+  f <- arbitrary @(Time -> (Bool, (Bool, Word8)))
+  t <- arbitrary @Bool
+  pure $
+    runCircuit (addressable snapN) ((V.singleton t, ) . f)
+      =-=
+        runCircuit snapN f
+
+main :: IO ()
+main = quickCheck addressableTest
+
+
+
+
 
 
 type family Yo (a :: k) :: Constraint where
@@ -76,17 +143,29 @@ data CircuitF a b where
   XorG :: CircuitF (Bool, Bool) Bool
   NotG :: CircuitF Bool Bool
   Always :: Show a => a -> CircuitF () a
+  Debug :: Show a => CircuitF a a
 
-  FoldrV
+  MapStateV
       :: KnownNat n
-      => Circuit () r
-      -> Circuit r ()
-      -> Circuit (a, r) (b, r)
-      -> CircuitF (Vec n a) (Vec n b)
+      => Circuit (a, r) (b, r)
+      -> CircuitF (Vec n a, r) (Vec n b, r)
+  FoldrV :: Circuit (a, b) b -> CircuitF (Vec n a, b) b
   CloneV :: KnownNat n => CircuitF a (Vec n a)
   ZipV :: KnownNat n => CircuitF (Vec n a, Vec n b) (Vec n (a, b))
 
   Together :: Circuit a b -> CircuitF a b
+  Embed :: Embeddable a => CircuitF a (Vec (Size a) Bool)
+  Reify :: Embeddable a => CircuitF (Vec (Size a) Bool) a
+
+  MapMaybe :: Circuit a b -> CircuitF (Maybe a) (Maybe b)
+  Uncons :: CircuitF (Vec (n + 1) a) (a, Vec n a)
+
+  Cross
+      :: (KnownNat (2 ^ (n + 1)))
+      => Circuit (a, a) a
+      -> CircuitF (Vec (n + 1) (a, a)) (Vec (2 ^ (n + 1)) a)
+
+
   -- Machine :: String -> [String] -> [String] -> Circuit a b -> CircuitF a b
   -- Label :: String -> Circuit a b -> CircuitF a b
 
@@ -253,7 +332,7 @@ raw = tell . pure . Raw
 --   raw "subgraph hi {"
 --   raw "rank=\"same\";"
 --   compile c i <* raw "}"
--- compile (LiftC (FoldrV create destroy each)) i = undefined
+-- compile (LiftC (MapStateV create destroy each)) i = undefined
 -- compile (LiftC (Always c)) i = undefined
 -- compile Distribute _ = undefined
 -- compile Factor _ = undefined
@@ -355,14 +434,36 @@ lowerCircuit = runFree $ \case
       let f = runRoar $ lowerCircuit k
        in Roar $ \tx t -> fst $ loop f tx t
     Together k -> lowerCircuit k
-    FoldrV create _ each -> Roar $ \fv t ->
-      let v = fv t
-          rc = runRoar (lowerCircuit create) (const ()) t
+    MapStateV each -> Roar $ \fv t ->
+      let (v, r) = fv t
           f = flip (runRoar (lowerCircuit each)) t
-       in foldrV (f . const) rc v
+       in foldrV (f . const) r v
     Always c -> Roar $ \ _ _ -> c
     CloneV -> Roar $ \ f nat -> V.repeat $ f nat
     ZipV -> Roar $ \f n -> uncurry zipV $ f n
+    Embed -> Roar $ \f n -> embed $ f n
+    Reify -> Roar $ \f n -> reify $ f n
+    Cross c -> Roar $ \f n ->
+      let v = f n
+          c' = flip (runRoar (lowerCircuit c)) n
+       in crossV (curry $ c' . const) v
+    FoldrV c -> Roar $ \f n ->
+      let (v, r) = f n
+          c' = flip (runRoar (lowerCircuit c)) n
+       in V.foldr (curry $ c' . const) r v
+    Debug -> Roar $ \f n -> trace (show $ f n) $ f n
+    MapMaybe m -> Roar $ \f n ->
+      let v = f n
+          m' = flip (runRoar (lowerCircuit m)) n
+       in fmap (m' . const) v
+    Uncons -> Roar $ \f n -> uncons $ f n
+
+uncons :: Vec (n + 1) a1 -> (a1, Vec n a1)
+uncons (a :> v) = (a, v)
+uncons _ = error "impossible"
+
+
+
 
 
 
@@ -412,11 +513,11 @@ zipV = V.zip
 --     -> Circuit (Vec n a) (Vec n b)
 -- foldrC create destroy one = recurseL $ peel >>> _
 
-foldrV :: ((a, r) -> (b, r)) -> r -> Vec n a -> Vec n b
-foldrV _ _ Nil = Nil
+foldrV :: ((a, r) -> (b, r)) -> r -> Vec n a -> (Vec n b, r)
+foldrV _ r Nil = (Nil, r)
 foldrV f r (Cons a vec) =
   let (b, r') = f (a, r)
-   in b :> foldrV f r' vec
+   in first (b :>) $ foldrV f r' vec
 
 
 
@@ -462,7 +563,7 @@ type Four = 4
 
 
 addN :: (Stuff n, PMU(n,(Bool)), PMU(n,(Bool,Bool))) => Circuit (Vec n (Bool, Bool)) (Vec n Bool)
-addN = LiftC $ FoldrV (always False) consume add
+addN = introduce >>> second' (always False) >>> LiftC (MapStateV add) >>> fst'
 
 
 split :: Circuit Bool (Bool, Bool)
@@ -471,6 +572,10 @@ split = id &&& notGate
 
 demux2 :: (Show a, Heyting a, Stuff a) => Circuit (a, Bool) (Either a a)
 demux2 = swap >>> tag
+
+-- newtype Addr n =
+
+-- memory :: Circuit (Vector n Bool
 
 
 introduce :: Stuff a => Circuit a (a, ())
@@ -495,17 +600,27 @@ shuffle = reassoc &&& (second' swap >>> reassoc)
       >>> (   (fst' >>> reassoc' >>> second' snd')
           *** (fst' >>> first' snd'))
 
+-- S
+-- V
 snap :: Circuit (Bool, Bool) Bool
 snap = (copy *** (split >>> swap)) >>> shuffle >>> (andGate *** andGate) >>> rsLatch
 
-clone :: (PMU(n,a), Stuff n, Stuff a) => Circuit a (Vec n a)
+clone :: (Stuff n, Stuff a) => Circuit a (Vec n a)
 clone = LiftC CloneV
 
-zip :: (PMU(n,a), PMU(n,b), PMU(n,(a,b)), Stuff n, Stuff a, Stuff b) => Circuit (Vec n a, Vec n b) (Vec n (a, b))
+zip :: (Stuff n, Stuff a, Stuff b) => Circuit (Vec n a, Vec n b) (Vec n (a, b))
 zip = LiftC ZipV
 
-snapN :: (PMU(n,Bool), PMU(n,(Bool,Bool)), Stuff n) => Circuit (Bool, Vec n Bool) (Vec n Bool)
-snapN = first' clone >>> zip >>> LiftC (FoldrV consume consume $ first' snap)
+snapN :: (Embeddable a, KnownNat (Size a), Stuff a) => Circuit (Bool, a) a
+snapN = second' (LiftC Embed)
+    >>> first' clone
+    >>> zip
+    >>> liftV snap
+    >>> LiftC Reify
+
+
+liftV :: (Stuff n, Stuff a, Stuff b) => Circuit a b -> Circuit (Vec n a) (Vec n b)
+liftV c = introduce >>> LiftC (MapStateV $ first' c) >>> fst'
 
 
 snapping :: Time -> (Bool, Bool)
@@ -515,10 +630,87 @@ snapping n = (False, odd n)
 
 type Time = Natural
 
-snappingN :: KnownNat n => Time -> (Bool, Vec n Bool)
-snappingN n@3 = (True, V.repeat $ odd n)
-snappingN n@6 = (True, V.repeat $ odd n)
-snappingN n = (False, V.repeat $ odd n)
+snappingN :: Time -> (Bool, (Bool, Word8))
+snappingN n = (False, (n < 256, fromIntegral $ n `mod` 255))
+
+
+type Addr n = Vec n Bool
+
+
+singleton :: Stuff a => Circuit a (Vec 1 a)
+singleton = clone
+
+
+addressable1
+    :: (Stuff a, Stuff b, Embeddable b, KnownNat (Size b))
+    => Circuit a b
+    -> Circuit (Bool, a) b
+addressable1 c
+    = first' split
+  >>> swap
+  >>> distribP
+  >>> yo *** yo
+  >>> zip
+  >>> liftV orGate
+  >>> LiftC Reify
+  where
+    yo = ((c >>> LiftC Embed) *** clone)
+     >>> zip
+     >>> liftV andGate
+
+distribV :: (Stuff a, Stuff b, KnownNat n) => Circuit (a, Vec n b) (Vec n (a, b))
+distribV = swap >>> LiftC (MapStateV $ swap >>> (id &&& fst')) >>> fst'
+
+
+debug :: (Stuff a, Show a) => Circuit a a
+debug = liftC Debug
+
+
+-- TODO(sandy): unsafe coercion! but it WORKS
+coerceBoolAMaybeA
+    :: forall a. (Stuff a, KnownNat (Size a), KnownNat (1 + Size a), Embeddable a)
+    => Circuit (Bool, a) (Maybe a)
+coerceBoolAMaybeA  = case proofNPlus1 @(Size a) of Refl -> LiftC Embed >>> LiftC Reify
+
+
+address
+    :: forall n a
+     . (KnownNat (Size a), Embeddable a, Show a, KnownNat (n + 1), Stuff a, KnownNat (2 ^ (n + 1)), KnownNat (1 + Size a))
+    => Circuit (Addr (n + 1), a)
+               (Vec (2 ^ (n + 1)) (Maybe a))
+address
+    = first' (liftV split >>> liftC (Cross andGate))
+  >>> swap
+  >>> distribV
+  >>> liftV (swap >>> coerceBoolAMaybeA)
+
+
+proofNPlus1 :: 1 + a :~: a + 1
+proofNPlus1 = unsafeCoerce Refl
+
+
+
+addressable
+    :: (Embeddable a, Show a, KnownNat (n + 1), Stuff a, Stuff b, Embeddable b, KnownNat (Size b), KnownNat (2 ^ (n + 1)), KnownNat (Size a), KnownNat (1 + Size a), KnownNat (Size b + 1))
+    => Circuit a b
+    -> Circuit (Addr (n + 1), a) b
+addressable c
+    = address
+  -- TODO(sandy): bug! we need to and-gate first!
+  >>> liftV (liftC (MapMaybe c) >>> liftC Embed)
+  >>> introduce
+  >>> second' (always False >>> clone)
+  >>> liftC (FoldrV $ zip >>> liftV orGate)
+  >>> liftC Uncons
+  >>> snd'
+  >>> liftC Reify
+
+
+addressableSnapping :: Time -> (Bool, (Bool, Bool))
+addressableSnapping n@3 = (False, (True, odd n))
+addressableSnapping n@5 = (True, (True, odd n))
+addressableSnapping n = (n `mod` 3 == 0, (False, odd n))
+
 
 
 
@@ -561,7 +753,33 @@ instance
     case t of
       False -> L1 $ greify $ V.takeI v
       True  -> R1 $ greify $ V.dropI @(GSize f) v
+  greify _ = error "imppossible"
 
+
+cross :: (a -> a -> a) -> [(a, a)] -> [a]
+cross _ [] = []
+cross _ [(a, b)] = [a, b]
+cross f ((a, b) : as) =
+  let c = cross f as
+   in fmap (f a) c <> fmap (f b) c
+
+
+crossV
+    :: (KnownNat (2 ^ (n + 1)))
+    => (a -> a -> a)
+    -> Vec (n + 1) (a, a)
+    -> Vec (2 ^ (n + 1)) a
+crossV f = V.unsafeFromList . cross f . V.toList
+  -- where
+  --   go Nil = Nil
+  --   go (Cons (a, b) vec) =
+  --     let vec' = go vec
+  --     in V.map (f a) vec' V.++ V.map (f b) vec'
+
+
+
+-- pairwiseV :: (Stuff a, Stuff b, KnownNat n) => Circuit (Vec n (a, b)) (Vec (2 ^ n) (a, b))
+-- pairwiseV = copy >>> liftC (MapStateV $ _) >>> snd'
 
 
 class Embeddable a where
@@ -585,6 +803,8 @@ instance Embeddable ()
 
 instance Embeddable Bool
 
+instance (KnownNat (Size a), Embeddable a) => Embeddable (Maybe a)
+
 instance
     (Embeddable a, Embeddable b, KnownNat (Size a))
       => Embeddable (a, b)
@@ -593,8 +813,18 @@ instance
     (Embeddable a, Embeddable b, KnownNat (Size a), KnownNat (Size b))
       => Embeddable (Either a b)
 
-data Test = A | B | C | D
-  deriving (Generic)
+instance (KnownNat n, Embeddable a, KnownNat (Size a)) => Embeddable (Vec n a) where
+  type Size (Vec n a) = n * Size a
+  embed = V.concatMap embed
+  reify = V.map reify . V.unconcatI
 
-instance Embeddable Test
+instance Embeddable Word8 where
+  type Size Word8 = 8
+  embed w8 =
+    let t = B.testBit w8
+     in t 0 :> t 1 :> t 2 :> t 3 :> t 4 :> t 5 :> t 6 :> t 7 :> Nil
+  reify (b0 :> b1 :> b2 :> b3 :> b4 :> b5 :> b6 :> b7 :> Nil) =
+    let s b = B.shiftL (bool 0 1 b)
+     in foldr @[] (B..|.) 0 $ zipWith s [b0, b1, b2, b3, b4, b5, b6, b7] [0..]
+  reify _ = error "impossible"
 
