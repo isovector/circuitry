53bdf50027d49ce52389658090fb8f64584e4c64
Sandy Maguire <sandy@sandymaguire.me>
2021-07-04 23:09:50 -0700

    Cleanup memspec

--- test/MemSpec.hs
+++ test/MemSpec.hs
@@ -1,20 +1,15 @@
 module MemSpec where
 
-import qualified Clash.Sized.Vector as V
-import qualified Data.Bits as B
-import           Data.Bool (bool)
-import           Data.Foldable hiding (sum)
 import           Prelude hiding ((.), id, sum)
-import           Take2.Computer.Examples
-import           Take2.Computer.Math
 import           Take2.Computer.Memory
-import           Take2.Computer.Simple
 import           Take2.Machinery
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 
+
 inputOverTime :: [a] -> Time -> a
 inputOverTime  as t = as !! fromIntegral t
+
 
 spec :: Spec
 spec = do
