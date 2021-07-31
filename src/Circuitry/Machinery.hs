393022d41f7fb709e62a6ee5da92e66026f27e80
Sandy Maguire <sandy@sandymaguire.me>
2021-07-30 22:13:12 -0700

    I have done some horrible things to make component sharing happen

--- src/Circuitry/Machinery.hs
+++ src/Circuitry/Machinery.hs
@@ -3,13 +3,8 @@ module Circuitry.Machinery
   , Vec (..)
   ) where
 
+import Prelude hiding (id)
 import Circuitry.Category as X
-import Circus.Types as X (renderModuleString)
-import Clash.Sized.Vector (Vec (..))
-import Data.Generics.Labels ()
-import Data.Word as X
-import GHC.Generics as X (Generic)
-import GHC.TypeLits as X (type (+), type (-), type (^), type (<=), KnownNat)
 import Circuitry.Circuit as X
 import Circuitry.Coproduct as X
 import Circuitry.Embed as X
@@ -18,7 +13,14 @@ import Circuitry.Instances as X
 import Circuitry.Numeric as X
 import Circuitry.Primitives as X (nandGate, mapFoldVC, zipVC, cloneV, fixC, foldVC, pad, diagrammed, binaryGateDiagram, unaryGateDiagram, unaryGateDiagram', transposeV, tribuf, crossV, unsafeShort, shortcircuit, pullDown, pullUp, traceC, replicateC)
 import Circuitry.Product as X
+import Circuitry.Shared as X
 import Circuitry.Signal as X (Time)
 import Circuitry.Word as X
+import Circus.Types as X (renderModuleString)
+import Clash.Sized.Vector (Vec (..))
+import Data.Generics.Labels ()
+import Data.Word as X
+import GHC.Generics as X (Generic)
+import GHC.TypeLits as X (type (+), type (-), type (^), type (<=), KnownNat)
 import Test.QuickCheck as X (Property, Arbitrary(..), CoArbitrary(..), Function(..), property, (===), forAllShrink, counterexample, quickCheck, resize, oneof, applyFun, functionMap)
 
---
+++ src/Circuitry/Shared.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE MagicHash #-}
+
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Circuitry.Shared where
+
+import           Circuitry.Circuit
+import           Circuitry.Embed
+import           Circuitry.Primitives
+import           Circuitry.Signal
+import qualified Clash.Sized.Vector as V
+import           Data.Function (fix)
+import           Data.IORef
+import           Data.Map (Map)
+import qualified Data.Map as M
+import           Data.Maybe (fromJust)
+import           Data.Word (Word8)
+import           System.IO.Unsafe (unsafePerformIO)
+import           Unsafe.Coerce (unsafeCoerce)
+import Circuitry.Graph
+import Circus.Types (Bit (..))
+import Circus.DSL (unifyBits)
+import Debug.Trace (traceM)
+
+
+instance Embed (a -> b) where
+  type SizeOf (a -> b) = 8
+
+data SomeSignal where
+  SomeSignal :: (Embed a, Embed b) => Signal a b -> SomeSignal
+
+sharedMap :: IORef (Map Word8 SomeSignal)
+sharedMap = unsafePerformIO $ newIORef mempty
+{-# NOINLINE sharedMap #-}
+
+sharedGraphMap :: IORef (Map Word8 ([Bit], [Bit]))
+sharedGraphMap = unsafePerformIO $ newIORef mempty
+{-# NOINLINE sharedGraphMap #-}
+
+
+share :: forall a b. (Embed a, Embed b) => Circuit a b -> Circuit () (a -> b)
+share c = unsafePerformIO $ do
+  ix <- fmap (fromIntegral . length) $ readIORef sharedMap
+  modifyIORef' sharedMap $ M.insert ix $ SomeSignal $ c_roar c
+  pure
+    $ primitive
+    $ Circuit (gr ix)
+    $ primSignal
+    $ const
+    $ fmap Just
+    $ embed ix
+  where
+    gr :: Word8 -> Graph () (a -> b)
+    gr ix = Graph $ \V.Nil -> do
+      a <- synthesizeBits @a
+      b <- unGraph (c_graph c) a
+      unsafePerformIO $ do
+        modifyIORef' sharedGraphMap $ M.insert ix (V.toList a, V.toList b)
+        pure $ pure $ V.repeat $ embedBit ix
+{-# NOINLINE share #-}
+
+embedBit :: Word8 -> Bit
+embedBit = Bit . fromIntegral
+
+eval :: forall a b. (Embed a, Embed b) => Circuit (a -> b, a) b
+eval
+    = Circuit gr
+    $ fix $ \me -> Signal
+    $ \v -> unsafePerformIO
+    $ do
+      let (ref, a) = V.splitAtI v
+          share_id = reify $ fromJust $ V.traverse# id ref
+      shared <- readIORef sharedMap
+      case shared M.! share_id of
+        SomeSignal sig -> do
+          let (sig', b) = pumpSignal (unsafeCoerce sig :: Signal a b) a
+          modifyIORef' sharedMap $ M.insert share_id $ SomeSignal sig'
+          pure (me, b)
+  where
+    gr :: Graph (a -> b, a) b
+    gr = Graph $ \v -> do
+      let (V.head -> Bit ref, a) = V.splitAtI @8 v
+          !ix = fromIntegral @_ @Word8 ref
+      unsafePerformIO $ do
+        (a', b) <- fmap (M.! ix) $ readIORef sharedGraphMap
+        pure $ do
+          unifyBits $ M.fromList $ zip (V.toList a) a'
+          pure $ V.unsafeFromList b
+
+{-# NOINLINE eval #-}
+
